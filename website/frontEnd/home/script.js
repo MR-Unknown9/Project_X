25cf1c77709892d4cec87fd4cb5f4aa5a27eac2d
Unknown <khaled.taha1918@gmail.com>
2025-05-22 04:56:54 +0300

    new version

--- website/frontEnd/home/script.js
+++
@@ -1,7 +0,0 @@
-// Dark Mode Toggle
-function toggleDarkMode() {
-  document.body.classList.toggle("dark");
-  const icon = document.getElementById("theme-icon");
-  icon.classList.toggle("fa-sun");
-  icon.classList.toggle("fa-moon");
-}
